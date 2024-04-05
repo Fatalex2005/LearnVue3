3ccb66a1de805396e709df4f68025b5f0a045bd5
Fatalex2005 <ruginleha@gmail.com>
2024-04-05 19:15:04 +0700

    Познакомились с работой API и динамическими маршрутами

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,10 @@
-<script setup>
-
-</script>
-
 <template>
 <header>
   <nav class="navbar navbar-dark bg-dark shadow p-3 ">
     <ul class="d-flex">
       <li class="nav-item"><RouterLink class="nav-link" to="/">Главная</RouterLink></li>
       <li class="nav-item"><RouterLink class="nav-link" to="/directives">Знакомство с директивами</RouterLink></li>
+      <li class="nav-item"><RouterLink class="nav-link" to="/api">Работа с API</RouterLink></li>
     </ul>
   </nav>
 </header>
---
+++ src/views/ApiView.vue
@@ -0,0 +1,41 @@
+<template>
+    <h2 class="text-center">Товары для чайников</h2>
+  <div class="d-flex flex-wrap justify-content-center p-4"></div>
+      <div class="card m-2" v-for="product in products">
+        <div class="card-header">{{product.title}}</div>
+        <div class="card-body">
+          <img :src="product.thumbnail" alt="">
+        </div>
+        <div class="card-footer d-flex justify-content-between">
+          <div class="fw-bold fst-italic">Цена: {{product.price}}$</div>
+          <div><RouterLink class="btn btn-dark" :to="'/api/' + product.id">Подробнее</RouterLink></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+  export default {
+    // Переменные
+    data() {
+      return{
+        products: [],
+      }
+    },
+    // Методы
+    methods: {
+      getProducts() {
+        fetch('https://dummyjson.com/products')
+            .then(response => response.json())
+            .then(data => this.products = data.products)
+      }
+    },
+    // Метод выполняется при загрузке страницы
+    created() {
+      this.getProducts();
+    }
+  }
+</script>
